f75c7f8f735f0099e51659641136b9080740891c
Aaron Clawson <madvikinggod@gmail.com>
2020-12-02 09:47:43 -0600

    day 2

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
 pub mod y2020;
 
+extern crate regex;
+
 #[cfg(test)]
 mod tests;
---
+++ src/y2020/day2/mod.rs
@@ -0,0 +1,32 @@
+use regex::Regex;
+
+pub fn read_input() -> Vec<(Rule, String)> {
+    let re = Regex::new(r"(\d+)-(\d+) (.): (.+)").unwrap();
+    let input = include_str!("input.txt");
+   input.lines()
+    .map(|line| re.captures(line).unwrap())
+    .map(|cap| (Rule::new(&cap[1],&cap[2],&cap[3]), String::from(&cap[4])))
+    .collect()
+}
+
+#[derive(Debug)]
+pub struct Rule {
+    min: usize,
+    max: usize,
+    charactor: char,
+}
+
+impl Rule {
+    pub fn new( min: &str, max: &str, charactor: &str) -> Rule {
+        Rule { min: min.parse().unwrap(), max: max.parse().unwrap(), charactor: charactor.chars().nth(0).unwrap()}
+    }
+    pub fn check_password(&self, password: String) -> bool {
+        let count = password.matches(self.charactor).count();
+        count>= self.min && count<= self.max
+    }
+    pub fn check_toboggan_password(&self, password: String) -> bool {
+        let c1 = password.chars().nth(self.min -1).unwrap() == self.charactor;
+        let c2 = password.chars().nth(self.max -1).unwrap() == self.charactor;
+        c1 ^ c2
+    }
+}
