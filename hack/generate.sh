93e5adbe45e7af800c2ca48047081e01bfdc29b8
Aaron Clawson <3766680+MadVikingGod@users.noreply.github.com>
2023-06-14 17:36:25 -0500

    day 4

--- hack/generate.sh
+++ hack/generate.sh
@@ -5,63 +5,26 @@ fi
 day="$(printf '%02d' $1)"
 
 if [ -z $2 ]; then
-    year="2021"
+    year="2022"
 else
     year="$2"
 fi
 
-bin_dir="src/bin/day$day-$year"
-test_dir="src/tests/y$year"
-lib_dir="src/y$year/day$day"
+year_dir="y$year"
+echo ${year_dir}
+bin_dir="${year_dir}/src/bin/day${day}"
 
-dirs="$bin_dir $test_dir $lib_dir"
-mkdir -p $dirs
-
-### Bins
+mkdir -p $bin_dir
 touch $bin_dir/instructions.txt
-grep -E "use advent_of_code::y$year::day$day::read_input;" $bin_dir/main.rs -q || cat >$bin_dir/main.rs <<EOF
-use advent_of_code::y$year::day$day::*;
-// Common tools
-// use advent_of_code::util::Field;
-// use advent_of_code::util::Point;
-// use std::collections::HashMap;
-// use std::collections::HashSet;
-// use std::collections::VecDeque;
-// use itertools::Itertools;
-
+touch $bin_dir/input.txt
+cat >$bin_dir/main.rs <<EOF
 fn main() {
     println!("Hello, world!");
 }
-EOF
 
-### Tests
-grep -E "y$year" src/tests/mod.rs -q || echo "mod y$year;" >> src/tests/mod.rs
-grep -E "day$day" $test_dir/mod.rs -q || echo "mod day$day;" >> $test_dir/mod.rs
-grep -E "use crate::y$year::day$day;" $test_dir/day$day.rs -q || cat > $test_dir/day$day.rs <<EOF
-use crate::y$year::day$day;
-
+#[cfg(test)]
 #[test]
-fn test_nothing()  -> Result<(),String> {
-    Ok(())
-}
-EOF
-
-### Library
-# Manually add `pub mod y$year;` ath the start of a new year
-grep -E "y$year" src/lib.rs -q || echo "Need to add 'pub mod y$year;' to src/lib.rs"
-grep -E "day$day" src/y$year/mod.rs -q || echo "pub mod day$day;" >> src/y$year/mod.rs
-touch $lib_dir/input.txt
-grep -E "pub fn read_input()" $lib_dir/mod.rs -q || cat >$lib_dir/mod.rs <<EOF
-// Common tools
-// use crate::util::Field;
-// use crate::util::Point;
-// use std::collections::HashMap;
-// use std::collections::HashSet;
-// use std::collections::VecDeque;
-// use itertools::Itertools;
-
-pub fn read_input() -> Vec<&'static str> {
-    let input = include_str!("input.txt");
-    input.lines().collect()
+fn test_nothing() {
+    ()
 }
 EOF
---
+++ y2022/src/bin/day04/main.rs
@@ -0,0 +1,60 @@
+fn main() {
+    let pairs = include_str!("input.txt").lines().map(|line| Pair::new(line.trim())).collect::<Vec<_>>();
+    let count = pairs.iter().filter(|pair| pair.contains()).count();
+    println!("Part 1: {}", count);
+    
+    let count = pairs.iter().filter(|pair| pair.overlap()).count();
+    println!("Part 2: {}", count);
+}
+
+struct Pair {
+    a: (u64, u64),
+    b: (u64, u64),
+}
+
+impl Pair {
+    fn new(input: &str) -> Self {
+        let mut iter = input.split(',').flat_map(|s| s.split("-").map(|s| s.parse::<u64>().unwrap()));
+        Self {
+            a: (iter.next().unwrap(), iter.next().unwrap()),
+            b: (iter.next().unwrap(), iter.next().unwrap()),
+        }
+    }
+
+    fn contains(&self) -> bool {
+        contains(self.a, self.b) || contains(self.b, self.a)
+    }
+    fn overlap(&self) -> bool {
+        overlap(self.a, self.b) || overlap(self.b, self.a)
+    }
+}
+
+fn contains(a: (u64, u64), b: (u64, u64)) -> bool {
+    a.0 <= b.0 && a.1 >= b.1
+}
+fn overlap(a: (u64, u64), b: (u64, u64)) -> bool {
+    a.0 <= b.1 && a.1 >= b.0
+}
+
+#[cfg(test)]
+#[test]
+fn test_pair_contains() {
+    let p = Pair::new("2-4,6-8");
+    assert_eq!(p.contains(), false);
+    assert_eq!(p.overlap(), false);
+    let p = Pair::new("2-3,4-5");
+    assert_eq!(p.contains(), false);
+    assert_eq!(p.overlap(), false);
+    let p = Pair::new("5-7,7-9");
+    assert_eq!(p.contains(), false);
+    assert_eq!(p.overlap(), true);
+    let p = Pair::new("2-8,3-7");
+    assert_eq!(p.contains(), true);
+    assert_eq!(p.overlap(), true);
+    let p = Pair::new("6-6,4-6");
+    assert_eq!(p.contains(), true);
+    assert_eq!(p.overlap(), true);
+    let p = Pair::new("2-6,4-8");
+    assert_eq!(p.contains(), false);
+    assert_eq!(p.overlap(), true);
+}
